fa069a8e1b7d3423728fcf2ded967adb6ea9e3ca
sunnycharkhwal1 <105035738+sunnycharkhwalhackinghunter@users.noreply.github.com>
2022-05-16 09:36:01 +0530

    1

--- src/modules/MaxHr/page/Dashboard.jsx
+++ src/modules/MaxHr/page/Dashboard.jsx
@@ -11,6 +11,15 @@ const Dashboard = () => {
         <UserName />
         <PagesTopTitle title="dashbaord" />
         <InfoCard />
+        <div>
+          <div className="row">
+            <div className="col-4">
+              <div className=" bg-white">
+                <p>Total Employees: 20</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
