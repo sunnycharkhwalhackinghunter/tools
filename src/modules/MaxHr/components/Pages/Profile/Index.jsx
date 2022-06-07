1e12953517cf8cbe9f96fd9748cccfd83ccb3a02
sunnycharkhwal1 <105035738+sunnycharkhwalhackinghunter@users.noreply.github.com>
2022-06-07 10:41:27 +0530

    1

--- src/modules/MaxHr/components/Pages/Profile/Index.jsx
+++ src/modules/MaxHr/components/Pages/Profile/Index.jsx
@@ -33,7 +33,9 @@ export const Tabs = () => {
           </Nav>
 
           <Tab.Content>
-            <Tab.Pane eventKey="first">1</Tab.Pane>
+            <Tab.Pane eventKey="first">
+              <EmployeesProfileCard />
+            </Tab.Pane>
             <Tab.Pane eventKey="second">
               <EmployeesCard />
             </Tab.Pane>
@@ -252,6 +254,56 @@ const EmployeesCard = () => {
                 />
               );
             })}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+const EmployeesProfileCard = () => {
+  return (
+    <>
+      <div className="EmployeesProfileCard_top_div">
+        <div className="row">
+          <div className="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">
+            <div className=" EmployeesProfileCard_icon_div_main">
+              <div className="EmployeesProfileCard_icon_div">
+                <ProfileIcon
+                  className="EmployeesProfileCard_icon"
+                  backgroundImage={ProfilPhoto2}
+                />
+              </div>
+              <div className="EmployeesProfileCard_text_div">
+                <h3>gaurav Kaushik</h3>
+                <p>
+                  hR Manager <span>ID: MXHR02</span>{" "}
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12 EmployeesProfileCard_div_info">
+            <div className="EmployeesProfileCard_info">
+              <p>
+                <span>
+                  <AiOutlineMail />
+                </span>
+                gaurav@maxlence.com.au
+              </p>
+              <p>
+                <span>
+                  <IoCallOutline />
+                </span>
+                +91 834 9933 768
+              </p>
+            </div>
+          </div>
+          <div className="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12 EmployeesProfileCard_div_info">
+            <div className="EmployeesProfileCard_info_user">
+              <p>
+                Reporting to:
+                <span> Shived Bansel</span>
+              </p>
+            </div>
           </div>
         </div>
       </div>
