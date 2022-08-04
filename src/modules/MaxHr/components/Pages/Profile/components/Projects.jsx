32e11a1b424c3be83e0a3a7c1d26c45e1d38d2c5
sunnycharkhwal1 <105035738+sunnycharkhwalhackinghunter@users.noreply.github.com>
2022-08-04 16:03:32 +0530

    4 aug 2022

--- src/modules/MaxHr/components/Pages/Profile/components/Projects.jsx
+++ src/modules/MaxHr/components/Pages/Profile/components/Projects.jsx
@@ -1,19 +1,24 @@
 import Input from "../../../../../common/components/input/Input";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-
+import ProfileIcon from "../../../../../common/components/img/Img";
+import ProfilPhoto2 from "../../../../../../pics/profile2.jpeg";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
 import React from "react";
-import TemporaryDrawer from "./page/Right";
+import { useState } from "react";
+import { RightFilters } from "../../../../../common/components/RightFilters/RightFilters";
+import { AboutProfileInput } from "./AboutProfile";
 export const Projects = () => {
   return (
     <>
       <ProjectsFilter />
+      <ProjectsCard />
     </>
   );
 };
 export const ProjectsFilter = () => {
-  const [modalShow, setModalShow] = React.useState(false);
-
   return (
     <>
       <div className=" mb-4">
@@ -33,7 +38,230 @@ export const ProjectsFilter = () => {
             </div>
           </div>
           <div className="col-2">
-            <TemporaryDrawer />
+            <RightFilters
+              title="Filter"
+              btn1="Reset"
+              btn2="Show results"
+              data={<ProjectsRightFiltersData />}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export const ProjectsRightFiltersData = () => {
+  const [FiltersAdd, setFiltersAdd] = useState(false);
+  const FiltersAddOpen = () => {
+    setFiltersAdd(!FiltersAdd);
+  };
+  const Data = [
+    {
+      toptile: "Sort By",
+      cat1: [
+        {
+          label: "Most recent",
+        },
+        {
+          label: "Most relevent",
+        },
+      ],
+    },
+    {
+      toptile: "Priority",
+      cat1: [
+        {
+          label: "High",
+        },
+        {
+          label: "Medium",
+        },
+        {
+          label: "Low",
+        },
+      ],
+    },
+    {
+      toptile: "Department",
+      cat1: [
+        {
+          label: "My role only",
+        },
+        {
+          label: "Design",
+        },
+        {
+          label: "Developement",
+        },
+        {
+          label: "Marketing",
+        },
+        {
+          label: "Others",
+        },
+      ],
+    },
+    {
+      toptile: "Status",
+      cat1: [
+        {
+          label: "Assigned",
+        },
+        {
+          label: "In progress",
+        },
+        {
+          label: "In review",
+        },
+        {
+          label: "Not started",
+        },
+        {
+          label: "Pending",
+        },
+        {
+          label: "Delayed",
+        },
+        {
+          label: "Completed",
+        },
+      ],
+    },
+    {
+      toptile: "Status",
+      cat1: [
+        {
+          label: "Assigned",
+        },
+        {
+          label: "In progress",
+        },
+        {
+          label: "In review",
+        },
+        {
+          label: "Not started",
+        },
+        {
+          label: "Pending",
+        },
+        {
+          label: "Delayed",
+        },
+        {
+          label: "Completed",
+        },
+      ],
+    },
+  ];
+  const App = (props) => {
+    return (
+      <>
+        <div className="row Projects_Right_Filters_Data_main_div">
+          <div className="col-12">
+            <p>{props.toptile}</p>
+          </div>
+          {props.arr.cat1.map((val, i) => {
+            return (
+              <div
+                key={i}
+                className="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12"
+              >
+                <FormGroup>
+                  <FormControlLabel control={<Checkbox />} label={val.label} />
+                </FormGroup>
+              </div>
+            );
+          })}
+        </div>
+      </>
+    );
+  };
+  const FiltersInnerBtn = () => {
+    return (
+      <>
+        <div className="row g-xxl-4 g-xl-4 g-lg-4 g-md-4 g-sm-3 g-3">
+          <div className="col-6">
+            <AboutProfileInput
+              label="Start Date"
+              type="date"
+              className="form-control"
+              placeholder="918-233-2345"
+            />
+          </div>
+          <div className="col-6">
+            <AboutProfileInput
+              label="End Date"
+              type="date"
+              className="form-control"
+              placeholder="918-233-2345"
+            />
+          </div>
+          <div className="col-12 text-center">
+            <div className={`${FiltersAdd ? "d-block" : "d-none"}`}>
+              <button
+                className="UploadDocumentModal_body_btn2 w-75"
+                onClick={FiltersAddOpen}
+              >
+                Add
+              </button>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  };
+  return (
+    <>
+      {Data.map((val, i) => {
+        return <App key={i} toptile={val.toptile} arr={val} />;
+      })}
+      <div className={`${FiltersAdd ? "d-none" : "d-block"}`}>
+        <button
+          className="UploadDocumentModal_body_btn2"
+          onClick={FiltersAddOpen}
+        >
+          Custom
+        </button>
+      </div>
+      <div>{FiltersAdd ? <FiltersInnerBtn /> : null}</div>
+    </>
+  );
+};
+const ProjectsCard = () => {
+  return (
+    <>
+      <div className="row">
+        <div className="col-3">
+          <div className="projects_card_div">
+            <h3>ABC Project Custom...</h3>
+            <p>
+              Last Update:<span>4 hr Ago</span>
+            </p>
+            <div className="projects_card_status">In Progress</div>
+            <ul className="projects_card_members">
+              <li>
+                <p>Members:</p>
+              </li>
+              <li className=" mx-3  d-flex">
+                <ProfileIcon
+                  className="projects_card_members_icon"
+                  backgroundImage={ProfilPhoto2}
+                />
+                <ProfileIcon
+                  className="projects_card_members_icon"
+                  backgroundImage={ProfilPhoto2}
+                />
+                <ProfileIcon
+                  className="projects_card_members_icon"
+                  backgroundImage={ProfilPhoto2}
+                />
+                <ProfileIcon
+                  className="projects_card_members_icon"
+                  backgroundImage={ProfilPhoto2}
+                />
+              </li>
+            </ul>
           </div>
         </div>
       </div>
---
+++ src/modules/common/components/RightFilters/RightFilters.jsx
@@ -0,0 +1,65 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import { GrFilter } from "react-icons/gr";
+import { IoCloseSharp } from "react-icons/io5";
+
+export const RightFilters = (props) => {
+  const [showFilters, setShowFilters] = React.useState(false);
+  return (
+    <>
+      <div>
+        <div className="Projects_btn_div" onClick={() => setShowFilters(true)}>
+          <p>Filters</p>
+          <span>
+            <GrFilter />
+          </span>
+        </div>
+        <Drawer
+          className="projact_right_filter_div"
+          anchor="right"
+          open={showFilters}
+        >
+          <Box role="presentation">
+            <div className="projact_right_filter_inner_div">
+              <div className="row">
+                <div className="col-6">
+                  <p className="projact_right_filter_title">{props.title}</p>
+                </div>
+                <div className="col-6 projact_right_filter_title_close_btn">
+                  <IoCloseSharp onClick={() => setShowFilters(false)} />
+                </div>
+                <div className="col-12">
+                  <div className="projact_right_filter_data">{props.data}</div>
+                </div>
+              </div>
+              <div className="row projact_right_filter_btn_div">
+                <div className="col-12">
+                  <div className="UploadDocumentModal_body_btn_div">
+                    <button className="UploadDocumentModal_body_btn">
+                      {props.btn1}
+                    </button>
+                    <button className="UploadDocumentModal_body_btn2">
+                      {props.btn2}
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Box>
+        </Drawer>
+      </div>
+    </>
+  );
+};
+// export const Right = () => {
+//   return (
+//     <>
+//       <footer class="demo-footer">
+//         <a href="http://www.bootpen.com" target="_blank">
+//           Get more code snippets
+//         </a>
+//       </footer>
+//     </>
+//   );
+// };
