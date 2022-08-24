ebcedf9d7202e36e602e52778161ecfc7f87959e
sunnycharkhwal1 <105035738+sunnycharkhwalhackinghunter@users.noreply.github.com>
2022-08-24 16:31:30 +0530

    1

--- src/modules/MaxHr/components/Pages/Profile/components/EmployeesOffBoard.jsx
+++ src/modules/MaxHr/components/Pages/Profile/components/EmployeesOffBoard.jsx
@@ -10,16 +10,17 @@ import GIF from "../../../../../common/components/img/Img";
 import GifPhoto from "../../../../../../pics/su.gif";
 export const EmployeesOffBoard = () => {
   const CollectAssetList = () => {
+    const [HandOverListShow, setHandOverListShow] = React.useState(false);
     const App = (props) => {
       return (
         <>
           <tr>
-            <td>{props.AssetsName}</td>
-            <td>{props.AssetsNumber}</td>
+            <td>{props.AssestName}</td>
+            <td>{props.AssestNumber}</td>
             <td>{props.Remarks}</td>
             <td>{props.IssueDate}</td>
-            <td>{props.ExpectedReturnDate}</td>
-            <td>{props.ActualReturnDate}</td>
+            <td>{props.ReturnDate}</td>
+            <td>{props.AcknowledgedBy}</td>
             <td>
               <div>
                 <input
@@ -34,74 +35,113 @@ export const EmployeesOffBoard = () => {
         </>
       );
     };
+    const HandOverListShowModal = (props) => {
+      return (
+        <Modal
+          {...props}
+          size="md"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Body className="UploadDocumentModal_body ExperienceEditModal_body">
+            <div className="UploadDocumentModal_body_close_btn">
+              <span onClick={props.onHide}>
+                <AiOutlineClose />
+              </span>
+            </div>
+            <div>
+              <div className=" d-flex">
+                <div>
+                  <div className="Experience_timline_title2">
+                    <h3>Hand Over</h3>
+                  </div>
+                </div>
+              </div>
+              <div className="ExperienceEditModal_text">
+                <div className="row g-xxl-4 g-xl-4 g-lg-4 g-md-4 g-sm-3 g-3">
+                  <div className="col-12">
+                    <AboutProfileInput
+                      label=" select Hand Over date "
+                      type="date"
+                      className="form-control"
+                      placeholder=" Title"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="UploadDocumentModal_body_btn_div">
+              <button
+                onClick={props.onHide}
+                className="UploadDocumentModal_body_btn2"
+              >
+                Return
+              </button>
+            </div>
+          </Modal.Body>
+        </Modal>
+      );
+    };
     const Data = [
       {
-        AssetsName: "Laptop",
-        AssetsNumber: "12339 cawcs13wr",
-        Remarks: "NA",
+        AssestName: "Laptop",
+        AssestNumber: "12339 cawcs13wr",
+        Remarks: "ID: OAieowhfh9w, Password: kuae837r0jl@$",
         IssueDate: "09/23/2021",
-        ExpectedReturnDate: "12/05/2021",
-        ActualReturnDate: "12/05/2021",
-      },
-      {
-        AssetsName: "Laptop",
-        AssetsNumber: "12339 cawcs13wr",
-        Remarks: "NA",
-        IssueDate: "09/23/2021",
-        ExpectedReturnDate: "12/05/2021",
-        ActualReturnDate: "12/05/2021",
-      },
-      {
-        AssetsName: "Laptop",
-        AssetsNumber: "12339 cawcs13wr",
-        Remarks: "NA",
-        IssueDate: "09/23/2021",
-        ExpectedReturnDate: "12/05/2021",
-        ActualReturnDate: "12/05/2021",
-      },
-      {
-        AssetsName: "Laptop",
-        AssetsNumber: "12339 cawcs13wr",
-        Remarks: "NA",
-        IssueDate: "09/23/2021",
-        ExpectedReturnDate: "12/05/2021",
-        ActualReturnDate: "12/05/2021",
-      },
-      {
-        AssetsName: "Laptop",
-        AssetsNumber: "12339 cawcs13wr",
-        Remarks: "NA",
-        IssueDate: "09/23/2021",
-        ExpectedReturnDate: "12/05/2021",
-        ActualReturnDate: "12/05/2021",
+        ReturnDate: "09/23/2021",
+        AcknowledgedBy: "Shived Bansal",
       },
     ];
     return (
       <>
         <div>
-          <table className="table text-center IssueAssets_top_div ">
+          <table className="table text-center HandOverList_top_div ">
             <thead>
               <tr>
-                <th scope="col">Assets Name</th>
-                <th scope="col">Assets Number</th>
+                <th scope="col">Assest Name</th>
+                <th scope="col">Asset Number</th>
                 <th scope="col">Remarks</th>
                 <th scope="col">Issue Date</th>
-                <th scope="col"> Return Date</th>
-                <th scope="col">acknowledged by</th>
-                <th>action</th>
+                <th scope="col">Return Date</th>
+                <th scope="col">Acknowledged By</th>
+                <th scope="col">Action</th>
               </tr>
             </thead>
             <tbody>
+              <tr>
+                <td>Maxlence HR APK</td>
+                <td>12339 cawcs13wr</td>
+                <td>ID: OAieowhfh9w, Password: kuae837r0jl@$</td>
+                <td>09/23/2021</td>
+                <td className="hand_over_list_date_icon">
+                  <MdDateRange onClick={() => setHandOverListShow(true)} />
+                  <HandOverListShowModal
+                    show={HandOverListShow}
+                    onHide={() => setHandOverListShow(false)}
+                  />
+                </td>
+                <td>Shived Bansal</td>
+                <td>
+                  <div>
+                    <input
+                      className="form-check-input"
+                      type="checkbox"
+                      value=""
+                      id="flexCheckChecked"
+                    />
+                  </div>
+                </td>
+              </tr>
               {Data.map((val, i) => {
                 return (
                   <App
                     key={i}
-                    AssetsName={val.AssetsName}
-                    AssetsNumber={val.AssetsNumber}
+                    AssestName={val.AssestName}
+                    AssestNumber={val.AssestNumber}
                     Remarks={val.Remarks}
                     IssueDate={val.IssueDate}
-                    ExpectedReturnDate={val.ExpectedReturnDate}
-                    ActualReturnDate={val.ActualReturnDate}
+                    ReturnDate={val.ReturnDate}
+                    AcknowledgedBy={val.AcknowledgedBy}
                   />
                 );
               })}
@@ -458,6 +498,151 @@ export const EmployeesOffBoard = () => {
       </>
     );
   };
+  const Action = () => {
+    const [ActionShow, setActionShow] = useState(false);
+
+    const ActionShowOpen = () => {
+      setActionShow(!ActionShow);
+    };
+    const ActionForm = () => {
+      return (
+        <>
+          <label className="all_page_my_label">
+            If everything goes well, It will be offboarded on 27th of Jan 2023
+          </label>
+        </>
+      );
+    };
+    const Approval = () => {
+      const App = (props) => {
+        return (
+          <>
+            <div className="col-12">
+              <div className="approval_title_main_div">
+                <div style={{ width: "8rem" }}>
+                  <div className="approval_title">
+                    <h3>{props.title}</h3>
+                  </div>
+                </div>
+                <div>
+                  <div className="approval_title2">
+                    <h3>{props.status}</h3>
+                  </div>
+                </div>
+                <div className=" w-100">
+                  <div className="setting_text setting_text2">{props.text}</div>
+                </div>
+              </div>
+            </div>
+          </>
+        );
+      };
+      const Data = [
+        {
+          title: "Admin 1",
+          status: "Approved",
+          text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet ",
+        },
+        {
+          title: "Admin 2",
+          status: "Pending",
+          text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet ",
+        },
+      ];
+      return (
+        <>
+          <div className="EmployeesDocument_top_title mt-4 mb-4">
+            <h3>Approval</h3>
+          </div>
+          <div className="row g-xxl-4 g-xl-4 g-lg-4 g-md-4 g-sm-3 g-3">
+            {Data.map((val, i) => {
+              return (
+                <App
+                  key={i}
+                  title={val.title}
+                  status={val.status}
+                  text={val.text}
+                />
+              );
+            })}
+            <div className="col-12">
+              <div className="approval_title_main_div">
+                <div style={{ width: "8rem" }}>
+                  <div className="approval_title">
+                    <h3>Admin 3</h3>
+                  </div>
+                </div>
+                <div>
+                  <div className="approval_title2">
+                    <button className="approve_input_btn">Approve</button>
+                  </div>
+                </div>
+                <div className="approve_input">
+                  <AboutProfileInput
+                    type="text"
+                    className="form-control"
+                    placeholder="Write your Feedback if any"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      );
+    };
+    return (
+      <>
+        <div className="row g-xxl-4 g-xl-4 g-lg-4 g-md-4 g-sm-3 g-3">
+          <div className="col-12">
+            <div>
+              <div className="form-check">
+                <input
+                  onClick={ActionShowOpen}
+                  className="form-check-input"
+                  type="checkbox"
+                  id="1"
+                />
+                <label className="form-check-label " htmlFor="1">
+                  Activate Resignation Process
+                </label>
+              </div>
+              {ActionShow ? <ActionForm /> : null}
+            </div>
+          </div>
+          <div className="col-12">
+            <label className="all_page_my_label">
+              Explanation from user for this step.
+            </label>
+            <div className="setting_text setting_text2">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
+              turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus
+              nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum
+              tellus elit sed risus. Maecenas eget condimentum velit, sit amet
+              feugiat lectus. Class aptent taciti sociosqu ad litora torquent
+              per conubia nostra, per inceptos himenaeos. Praesent auctor purus
+              luctus enim egestas, ac scelerisque ante pulvinar. Donec ut
+              rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur
+              vel bibendum lorem. Morbi convallis convallis diam sit amet
+              lacinia. Aliquam in elementum tellu
+            </div>
+            <div className="action_doc_div">
+              <div className="skills_main_div ">
+                <div className="skills_main_inner_div">
+                  <span>Screen shots</span>
+                </div>
+                <div className="skills_main_inner_div">
+                  <span>Others</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-12">
+            <Approval />
+          </div>
+        </div>
+      </>
+    );
+  };
   return (
     <>
       <div className="profile_iner_cont assets_tab_div">
@@ -510,100 +695,6 @@ export const EmployeesOffBoard = () => {
               </Tab.Pane>
             </Tab.Content>
           </Tab.Container>
-        </div>
-      </div>
-    </>
-  );
-};
-const Action = () => {
-  const [ActionShow, setActionShow] = useState(false);
-
-  const ActionShowOpen = () => {
-    setActionShow(!ActionShow);
-  };
-  const ActionForm = () => {
-    return (
-      <>
-        <label className="all_page_my_label">
-          If everything goes well, It will be offboarded on 27th of Jan 2023
-        </label>
-      </>
-    );
-  };
-  const Approval = () => {
-    return (
-      <>
-        <div className="EmployeesDocument_top_title mt-4 mb-4">
-          <h3>Approval</h3>
-        </div>
-        <div className="approval_title_main_div">
-          <div style={{ width: "8rem" }}>
-            <div className="approval_title">
-              <h3>Admin 1</h3>
-            </div>
-          </div>
-          <div>
-            <div className="approval_title2">
-              <h3>Approved</h3>
-            </div>
-          </div>
-          <div className=" w-100">
-            <div className="setting_text setting_text2">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-              vulputate libero et velit interdum, ac aliquet{" "}
-            </div>
-          </div>
-        </div>
-      </>
-    );
-  };
-  return (
-    <>
-      <div className="row g-xxl-4 g-xl-4 g-lg-4 g-md-4 g-sm-3 g-3">
-        <div className="col-12">
-          <div>
-            <div className="form-check">
-              <input className="form-check-input" type="checkbox" id="1" />
-              <label
-                className="form-check-label "
-                htmlFor="1"
-                onClick={ActionShowOpen}
-              >
-                Activate Resignation Process
-              </label>
-            </div>
-            {ActionShow ? <ActionForm /> : null}
-          </div>
-        </div>
-        <div className="col-12">
-          <label className="all_page_my_label">
-            Explanation from user for this step.
-          </label>
-          <div className="setting_text setting_text2">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
-            turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus
-            nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum
-            tellus elit sed risus. Maecenas eget condimentum velit, sit amet
-            feugiat lectus. Class aptent taciti sociosqu ad litora torquent per
-            conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus
-            enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex.
-            Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum
-            lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in
-            elementum tellu
-          </div>
-          <div className="action_doc_div">
-            <div className="skills_main_div ">
-              <div className="skills_main_inner_div">
-                <span>Screen shots</span>
-              </div>
-              <div className="skills_main_inner_div">
-                <span>Others</span>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col-12">
-          <Approval />
         </div>
       </div>
     </>
